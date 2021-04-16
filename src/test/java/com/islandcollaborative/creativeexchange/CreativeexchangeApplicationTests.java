1046f04a288145b6d265854a8fb44f04f4813811
Ed <houedward@gmail.com>
2021-04-16 14:26:23 -0700

    initialized spring and gradle project

---
+++ src/test/java/com/islandcollaborative/creativeexchange/CreativeexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islandcollaborative.creativeexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
