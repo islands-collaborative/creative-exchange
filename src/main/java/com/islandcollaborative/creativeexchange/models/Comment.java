f7090e3988176d386c9e0cfef0df53fa5a67e933
James Mansour <jamestmansour@outlook.com>
2021-04-19 10:12:51 -0700

    Enables Comment repository to start.

--- src/main/java/com/islandcollaborative/creativeexchange/models/Comment.java
+++ src/main/java/com/islandcollaborative/creativeexchange/models/Comment.java
@@ -3,6 +3,10 @@ package com.islandcollaborative.creativeexchange.models;
 import javax.persistence.*;
 import java.util.*;
 
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
 
 }
