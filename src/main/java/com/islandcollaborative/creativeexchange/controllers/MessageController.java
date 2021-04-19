226bd57e20db0cd3f14e0ea716c114f7be2c6851
James Mansour <jamestmansour@outlook.com>
2021-04-19 11:53:40 -0700

    Adds basic logic for all non Stretch goals to the MessageController.

--- src/main/java/com/islandcollaborative/creativeexchange/controllers/MessageController.java
+++ src/main/java/com/islandcollaborative/creativeexchange/controllers/MessageController.java
@@ -1,27 +1,75 @@
 package com.islandcollaborative.creativeexchange.controllers;
 
+import com.islandcollaborative.creativeexchange.models.AppUser;
+import com.islandcollaborative.creativeexchange.models.Message;
+import com.islandcollaborative.creativeexchange.repositories.AppUserRepository;
+import com.islandcollaborative.creativeexchange.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MessageController {
 
-    // - `GET /messages`: Requires authentication. "Inbox view". Displays a list of message threads that the currently logged in user is a part of. Sorts the threads by how recent a message was posted and whether there are unread messages.
+    @Autowired
+    AppUserRepository appUserRepository;
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    /**
+     * @return htmlPage
+     * GET "/messages"
+     * Requires authentication.
+     * <p>
+     * "Inbox view". Displays a list of message threads that the currently logged in user
+     * is a part of. Sorts the threads by how recent a message was posted and whether there
+     * are unread messages.
+     */
     @GetMapping("/messages")
-    public String getInbox() {
+    public String getInbox(HttpServletRequest request, Model m) {
+        AppUser userPrincipal = appUserRepository.findByUsername(request.getUserPrincipal().getName());
+        m.addAttribute("messages", userPrincipal.getThreads());
         return "inbox";
     }
 
-    // - `GET /users/{userId}/messages`: Requires authentication. Displays a thread of the messages between the currently logged in user and the subject user indicated by the `{userId}` parameter. Also provides a form for the user to send a message in that thread.
+    /**
+     * @param userId id of user who's conversation to show.
+     * @return htmlPage
+     * GET "/messages"
+     * Requires authentication.
+     * <p>
+     * Displays a thread of the messages between the currently logged in user and the subject
+     * user indicated by the `{userId}` parameter. Also provides a form for the user to send a
+     * message in that thread.
+     */
     @GetMapping("/users/{userId}/messages")
-    public String getThread(@PathVariable long userId) {
+    public String getThread(@PathVariable long userId, HttpServletRequest request, Model m) {
+        AppUser userPrincipal = appUserRepository.findByUsername(request.getUserPrincipal().getName());
+        m.addAttribute("messages", userPrincipal.getMessageThread(userId));
         return "thread";
     }
 
-    // - `POST /users/{userId}/messages`: Requires authentication. Sends a message from the currently logged in user to the subject user. Redirects to the message thread at `/users/{userId}/messages`.
+    /**
+     * @param userId AppUser to send the message to.
+     * @param text content of the message to be sent.
+     * @return Redirects to "/users/{userId}/messages"
+     * Post /users/{userId}/messages
+     * Requires authentication
+     *
+     * Sends a message from the currently logged in user to the subject user. Redirects to the
+     * message thread at `/users/{userId}/messages`.
+     */
     @PostMapping("/users/{userId}/messages")
-    public RedirectView sendMessage(@PathVariable("userId") long userId) {
+    public RedirectView sendMessage(@PathVariable("userId") long userId, HttpServletRequest request, Model m, String text) {
+        AppUser userPrincipal = appUserRepository.findByUsername(request.getUserPrincipal().getName());
+        AppUser recipient = appUserRepository.getOne(userId);
+        Message message = new Message(userPrincipal, recipient, text);
+        messageRepository.save(message);
         return new RedirectView("/users/" + userId + "/messages");
     }
 
