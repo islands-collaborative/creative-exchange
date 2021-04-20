bd225d3def427f16e5162d2e13ca557024fd1d55
Andy Agulue <andyfandandy@gmail.com>
2021-04-20 09:08:04 -0700

    adds initial styling

--- src/main/java/com/islandcollaborative/creativeexchange/configs/WebSecurityConfig.java
+++ src/main/java/com/islandcollaborative/creativeexchange/configs/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/signup", "/login", "/about").permitAll()
-//                .antMatchers("/css/**").permitAll()
+               .antMatchers("../css/**").permitAll()
                 .antMatchers("/assets/**").permitAll()
                 .anyRequest().authenticated()
 
