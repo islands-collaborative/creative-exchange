b933fa92a554adbe86c87337c64f06fa31710434
Seamus Brown <shay_brown_13@hotmail.com>
2021-04-21 11:56:50 -0700

    about us first pass

--- src/main/java/com/islandcollaborative/creativeexchange/models/AppUser.java
+++ src/main/java/com/islandcollaborative/creativeexchange/models/AppUser.java
@@ -27,6 +27,8 @@ public class AppUser implements UserDetails {
 
     //TODO Picture
 
+
+
     @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Message> sentMessages = new ArrayList<>();
     @OneToMany(mappedBy = "recipient", cascade = CascadeType.ALL, orphanRemoval = true)
