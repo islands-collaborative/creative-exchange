ae7ce96573d24581d66d35312673d736248e5ad6
James Mansour <jamestmansour@outlook.com>
2021-04-21 23:11:24 -0700

    Impliment error handelling

--- src/main/java/com/islandcollaborative/creativeexchange/controllers/MainController.java
+++ src/main/java/com/islandcollaborative/creativeexchange/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.islandcollaborative.creativeexchange.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
@@ -23,5 +26,22 @@ public class MainController {
     @GetMapping("/privacy")
     public String getPrivacy() {
         return "privacy";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error500";
+            }
+        }
+        return "error";
     }
 }
